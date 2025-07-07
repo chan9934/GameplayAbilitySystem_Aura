7d02d6890c731e52a953e43a9c45f22855e7a824
chan9934 <kim75249934@gmail.com>
2025-07-07 20:14:03 +0900

    [add] Add AuraEffectActor

---
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraEffectActor.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class AURA_API AAuraEffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAuraEffectActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
+	UFUNCTION()
+	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                            const FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
