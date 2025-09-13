45f345621daacbbe4f8e058ba6a88bd8e72febba
chan9934 <kim75249934@gmail.com>
2025-09-13 13:22:59 +0900

    [update] Adding XP to the Player State

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -25,10 +25,41 @@ UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
 void AAuraPlayerState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-	
+
 	DOREPLIFETIME(AAuraPlayerState, Level);
+	DOREPLIFETIME(AAuraPlayerState, XP);
 }
 
-void AAuraPlayerState::OnRep_Level()
+void AAuraPlayerState::SetLevel(int NewLevel)
 {
+	Level = NewLevel;
+	OnLevelChangedDelegate.Broadcast(NewLevel);
+}
+
+void AAuraPlayerState::AddToLevel(int AddLevel)
+{
+	Level += AddLevel;
+	OnLevelChangedDelegate.Broadcast(AddLevel);
+}
+
+void AAuraPlayerState::SetXP(int NewXP)
+{
+	XP = NewXP;
+	OnXPChangedDelegate.Broadcast(NewXP);
+}
+
+void AAuraPlayerState::AddToXP(int AddXP)
+{
+	XP += AddXP;
+	OnXPChangedDelegate.Broadcast(AddXP);
+}
+
+void AAuraPlayerState::OnRep_Level(int32 OldLEvel)
+{
+	OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void AAuraPlayerState::OnRep_XP(int32 OldXP)
+{
+	OnXPChangedDelegate.Broadcast(XP);
 }
--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -12,6 +12,7 @@ class UAttributeSet;
 /**
  * 
  */
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32)
 UCLASS()
 class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
 {
@@ -22,7 +23,21 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
 	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
+
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+	
 	FORCEINLINE int32 GetPlayerLevel()const { return Level;}
+	FORCEINLINE int32 GetPlayerXP()const { return XP;}
+
+	UFUNCTION(BlueprintCallable)
+	void SetLevel(int NewLevel);
+	UFUNCTION(BlueprintCallable)
+	void AddToLevel(int AddLevel);
+	UFUNCTION(BlueprintCallable)
+	void SetXP(int NewXP);
+	UFUNCTION(BlueprintCallable)
+	void AddToXP(int AddXP);
 
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -33,7 +48,11 @@ protected:
 private:
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level)
 	int32 Level = 1;
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_XP)
+	int32 XP = 0;
 
 	UFUNCTION()
-	void OnRep_Level();
+	void OnRep_Level(int32 OldLevel);
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
 };
